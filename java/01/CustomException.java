d2b17969710b06dcd0efc648aeec1491d3dcca3c
Gaurav <gaurav.ssnl@gmail.com>
2020-10-18 00:13:58 +0530

    User-Defined Exceptions example added.

---
+++ java/01/CustomException.java
@@ -0,0 +1,29 @@
+public class CustomException {
+    public static void divide(int x, int y) throws MyDivException {
+        if (y == 0)
+            throw new MyDivException("Divisor cannot be 0.");
+        System.out.println(x / y);
+    }
+
+    public static void main(String[] args) {
+        try {
+            divide(10, 3);
+            divide(10, 0);
+        } catch (MyDivException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+class MyDivException extends Exception {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    public MyDivException(String message) {
+        super(message);
+    }
+
+}
