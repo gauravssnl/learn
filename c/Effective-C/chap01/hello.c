2bcc48fe27d7388d6bdbe86dd409cb2010c54242
Gaurav <gaurav.ssnl@gmail.com>
2021-11-19 16:25:33 +0530

    Add hello world printf count.

--- c/Effective-C/chap01/hello.c
+++ c/Effective-C/chap01/hello.c
@@ -5,5 +5,7 @@ int main(void) {
     if (puts("Hello, world.C and Rust are cool.") == EOF) {
         return EXIT_FAILURE;
     }
+    int chrCount = printf("%s\n", "Hello, World");
+    printf("No. of characters: %d\n", chrCount);
     return EXIT_SUCCESS;
 }
