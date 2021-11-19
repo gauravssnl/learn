2499179b558bb50f637147117a7f394efb83fe2b
Gaurav <gaurav.ssnl@gmail.com>
2021-11-19 23:11:08 +0530

    Add enum example.

---
+++ c/Effective-C/chap02/enums.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+enum day { sun, mon, tues, wed, thu, fri, sat};
+enum cardinal_points { north = 0, east = 90, south = 180, west = 270};
+enum months { jan = 1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec};
+
+int main(int argc, char const *argv[])
+{   
+    enum day tues;
+    printf("%d\n", sun);
+    printf("%d\n", south);
+    printf("%d\n", nov);
+    return 0;
+}
